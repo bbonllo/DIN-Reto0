837529bdba3e5b7d0c131e985b9fa21e61c0cd2f
BBonLLo <65909299+BBonLLo@users.noreply.github.com>
2022-09-29 12:35:49 +0200

    JavaDoc Comments done 4

--- test/din/reto0/model/ViewFactoryTest.java
+++ test/din/reto0/model/ViewFactoryTest.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package din.reto0.model;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import din.reto0.exceptionManager.ExceptionManager;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -25,7 +17,7 @@ public class ViewFactoryTest {
      * Test of getView method, of class ViewFactory.
      */
     @Test
-    public void testGetView() {
+    public void testGetView() throws ExceptionManager{
         View v = ViewFactory.getView();
         
         assertNotNull(v);
